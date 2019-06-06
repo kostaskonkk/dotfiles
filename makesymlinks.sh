cf9a1bb0f335980e2bc5a4e3ce7467663fc724cd
Kostantinos Konstantinidis <kostaskonkk@gmail.com>
2019-06-06 21:04:36 +0200

    sym link for aliases

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -14,7 +14,7 @@ files="bashrc vimrc aliases" 		  # list of files/folders to symlink in homedir
 
 # create dotfiles_old in homedir
 echo "Creating $.olddir for backup of any existing dotfiles in ~"
-mkdir -p $.olddir
+mkdir -p $olddir
 echo "...done"
 
 # change to the dotfiles directory
@@ -23,7 +23,7 @@ cd $dir
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.$file ~/.dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
